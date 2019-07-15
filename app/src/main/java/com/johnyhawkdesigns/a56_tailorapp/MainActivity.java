f6e67c4dc5d23aa54b3afcb05da24c97e9a8f938
Ahsan <johnyhawkahsan@gmail.com>
2019-07-15 15:41:16 +0500

    2nd commit- Working on Drawer Layout

--- app/src/main/java/com/johnyhawkdesigns/a56_tailorapp/MainActivity.java
+++ app/src/main/java/com/johnyhawkdesigns/a56_tailorapp/MainActivity.java
@@ -1,14 +1,25 @@
 package com.johnyhawkdesigns.a56_tailorapp;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Tutorial for Drawer layout
+    // https://medium.com/quick-code/android-navigation-drawer-e80f7fc2594f
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle t;
+    private NavigationView nv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
 
